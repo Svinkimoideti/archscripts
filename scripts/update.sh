805afb616e084fbb9b18f01a18d192724e553b7e
Lawliet <132742220+Svinkimoideti@users.noreply.github.com>
2026-06-02 09:51:42 +0500

    Create update.sh

---
+++ scripts/update.sh
@@ -0,0 +1,5 @@
+yay -Syu --noconfirm &>/dev/null
+cd /home/arch/zapret-discord-youtube-linux/ && git pull &>/dev/null
+cd /home/arch/tg-ws-proxy/ && git pull &>/dev/null
+flatpak update &>/dev/null
+echo "Updated!"
