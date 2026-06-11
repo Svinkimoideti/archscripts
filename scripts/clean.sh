e1fd60d56220e15902aaef7a69d47615590bb325
Lawliet <132742220+Svinkimoideti@users.noreply.github.com>
2026-06-11 15:29:24 +0500

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,15 +1,13 @@
-clear
-read -p "Do you want to clear your system? [Y/n] " -n 1 -r
-echo
+read -p "Start clean? [Y/n] " -n 1 -r
 if [[ $REPLY =~ ^[Nn]$ ]]; then
   exit
 else
-  for i in "." ".." "..."; do echo -ne "\rStart cleaning $i "; sleep 0.1; done; echo
-  sudo journalctl --vacuum-time=2d &>/dev/null
-  if [ -d "/tmp" ]; then
-    sudo rm -rf /tmp/*
-  fi
-  sudo pacman -Rns $(pacman -Qdtq) --noconfirm &>/dev/null
-  yay -Scc --noconfirm &>/dev/null
+  for i in "." ".." "..."; do echo -ne "\rStart cleaning $i "; sleep 0.1; done;
+  echo ""
+  sudo journalctl --vacuum-time=2d &>/dev/null && echo "journalctl cleaned" || echo "journal error"
+  sudo rm -rf /tmp/* && echo "tmp cleaned" || echo "tmp cleaningerror"
+  yay -Scc --noconfirm &>/dev/null && echo "yay packages cleaned" || echo "yay -Scc -> error"
+  rm -rf ~/.zsh_history && touch ~/.zsh_history && echo "zsh history cleaned" || echo "zsh cleaning error"
 fi
-clear && echo "Cleaning done!"
+echo
+echo "Cleaning done!"
