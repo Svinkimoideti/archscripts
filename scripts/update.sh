8b7cf2ceb501beef1ea19afc85dbb53d2ec13857
Lawliet <132742220+Svinkimoideti@users.noreply.github.com>
2026-06-11 15:24:53 +0500

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,6 @@
-yay -Syu --noconfirm &>/dev/null
-cd /home/arch/zapret-discord-youtube-linux/ && git pull &>/dev/null
-cd /home/arch/tg-ws-proxy/ && git pull &>/dev/null
-flatpak update &>/dev/null
+clear
+yay -Syu --noconfirm &>/dev/null && echo 'yay updated' || echo "yay not updated"
+cd /home/arch/zapret-discord-youtube-linux/ && git pull &>/dev/null && echo 'zapret updated'
+cd /home/arch/tg-ws-proxy/ && git pull &>/dev/null && echo 'proxy updated'
+flatpak update &>/dev/null && echo 'flatpak updated'
 echo "Updated!"
