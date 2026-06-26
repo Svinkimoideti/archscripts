307e9f7fb830e91bafb81584375b5ad929429d88
Lawliet <132742220+Svinkimoideti@users.noreply.github.com>
2026-06-26 11:34:03 +0500

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,13 +1,12 @@
-read -p "Start clean? [Y/n] " -n 1 -r
+read -p "clean ctl/yay/tmp? [Y/n] " -n 1 -r
 if [[ $REPLY =~ ^[Nn]$ ]]; then
   exit
 else
-  for i in "." ".." "..."; do echo -ne "\rStart cleaning $i "; sleep 0.1; done;
+  for i in "." ".." "..."; do echo -ne "\rstarting $i "; sleep 0.1; done;
   echo ""
   sudo journalctl --vacuum-time=2d &>/dev/null && echo "journalctl cleaned" || echo "journal error"
   sudo rm -rf /tmp/* && echo "tmp cleaned" || echo "tmp cleaningerror"
   yay -Scc --noconfirm &>/dev/null && echo "yay packages cleaned" || echo "yay -Scc -> error"
-  rm -rf ~/.zsh_history && touch ~/.zsh_history && echo "zsh history cleaned" || echo "zsh cleaning error"
 fi
 echo
 echo "Cleaning done!"
